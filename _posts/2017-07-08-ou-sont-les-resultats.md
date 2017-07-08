e6fa15a412dd282ae3b40548f5622dc26f5d637f
Julien Roger <jujulroger@gmail.com>
2017-07-08 12:22:37 +0200

    color map tests

---
+++ _posts/2017-07-08-ou-sont-les-resultats.md
@@ -0,0 +1,11 @@
+---
+layout: single
+title:  "Où sont les résultats ?"
+header:
+  image: /assets/images/default.png
+date:   2017-07-05 12:22:43 +0200
+categories: meta
+tags: élections données prospection
+---
+
+Il est toujours fascinant de naviguer parmi les multiples tentatives de l'état de fournir ses données. Pour les résultats des élections la conclusion temporaire semble être : Mille sources, mille formats, rien de directement exploitable. À suivre...
--- assets/js/test-d3.js
+++ assets/js/test-d3.js
@@ -1,25 +1,115 @@
-var width = 550, height = 550;
+//uses tooltip.css and colorbrew.css
+
+var width = 700, height = 550;
+
+var map = baseurl + '/assets/maps/france-geojson/departements.geojson'
+
+var column_header = "% Voix/Ins_Macron"
+
+
+//projections data
+var center = [2.454071, 46.279229];
+var scale = 2600;
+var translation = [width / 2, height / 2];
 
 var path = d3.geoPath();
 
+//projection
 var projection = d3.geoConicConformal()
-    .center([2.454071, 46.279229])
-    .scale(2600)
-    .translate([width / 2, height / 2]);
+    .center(center)
+    .scale(scale)
+    .translate(translation);
 
 path.projection(projection);
 
+//svg placement selection
 var svg = d3.select('#example').append("svg")
     .attr("id", "svg")
     .attr("width", width)
-    .attr("height", height);
+    .attr("height", height)
+    .attr("class", "Blues");
 
 var deps = svg.append("g");
 
-d3.json(baseurl + '/assets/france-geojson-master/departements.geojson', function(req, geojson) {
-    deps.selectAll("path")
+//create tooltip element
+var div = d3.select("#example").append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
+
+//map creation
+d3.json(map, function(req, geojson) {
+
+    //draw (load ?) the map
+    var features  = deps.selectAll("path")
         .data(geojson.features)
         .enter()
         .append("path")
+        .attr('id', function(d) {return "d" + d.properties.code;})
         .attr("d", path);
+
+    //note: needs to be inside d3.json to load after the map
+    d3.csv(baseurl + "/assets/data/Presidentielle_2017_Resultats_Tour_2_Departement.csv", function(csv) {
+
+      //create quantile scale
+      var quantile = d3.scaleQuantile()
+          .domain([0, d3.max(csv, function(e) { return +e[column_header]; })])
+          .range(d3.range(9));
+
+      //create legend element to the side
+      var legend = svg.append('g')
+          .attr('transform', 'translate(525, 150)')
+          .attr('id', 'legend');
+
+      //add rectangles to legend
+      legend.selectAll('.colorbar')
+              .data(d3.range(9))
+            .enter().append('svg:rect')
+              .attr('y', function(d) { return d * 20 + 'px'; })
+              .attr('height', '20px')
+              .attr('width', '20px')
+              .attr('x', '0px')
+              .attr("class", function(d) { return "q" + d + "-9"; });
+
+      //add color scale to legend
+      var legendScale = d3.scaleLinear()
+          .domain([d3.min(csv, function(e) { return +e[column_header]; }), d3.max(csv, function(e) { return +e[column_header]; })])
+          .range([0, 9 * 20]);
+
+      //add axis to legend
+      var legendAxis = svg.append("g")
+          .attr('transform', 'translate(550, 150)')
+          .call(d3.axisRight(legendScale).ticks(6));
+
+      //color map according to scale and add tooltip
+      csv.forEach(function(e,i) {
+        d3.select("#d" + e["Code du département"])
+
+            //add colors
+            .attr("class", function(d) { return "department q" + quantile(+e[column_header]) + "-9"; })
+
+            //add tooltip
+            .on("mouseover", function(d) {
+                div.transition()
+                    .duration(50)
+                    .style("opacity", .9);
+
+                //tooltip html
+                div.html("<b>Département : </b>" + e["Libellé du département"] + "<br>"
+                      + "<b>" + column_header + ": </b>" + e[column_header] + "<br>"
+                      )
+                    .style("left", (d3.event.pageX + 30) + "px")
+                    .style("top", (d3.event.pageY - 30) + "px");
+            })
+            .on("mouseout", function(d) {
+                div.transition()
+                    .duration(500)
+                    .style("opacity", 0);
+                div.html("")
+                    .style("left", "0px")
+                    .style("top", "0px");
+            });
+
+    });
+
+    });
 });
